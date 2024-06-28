c899fb19be98bb7c262fed9c79383368bed4774e
Malottey1 <malcolm.clottey@ashesi.edu.gh>
2024-06-28 12:42:05 +0000

    "Added sweet alert dialog for registration and login, refactored login and register screens, and added API connection for login"

--- reverie_app/lib/services/api_connection.dart
+++ reverie_app/lib/services/api_connection.dart
@@ -23,4 +23,24 @@ class ApiConnection {
       throw Exception('Failed to register user');
     }
   }
+
+  Future<Map<String, dynamic>> loginUser(Map<String, String> credentials) async {
+    final response = await http.post(
+      Uri.parse(host + 'login.php'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(credentials),
+    );
+
+    // Print the request and response for debugging
+    print('Request URL: ${host + 'login.php'}');
+    print('Request Body: ${json.encode(credentials)}');
+    print('Response Status Code: ${response.statusCode}');
+    print('Response Body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to login user');
+    }
+  }
 }
